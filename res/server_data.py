1c57bc305711902a90e46211d0aa0226e5c72d06
Melissa Zapatero <martazapateroweb@gmail.com>
2025-08-15 00:19:32 +0200

    Create server_data.py as an example

---
+++ res/server_data.py
@@ -0,0 +1,9 @@
+# Discord parameters
+SERVER_NAME = "your-server-name-here"         # Doesn't need to be accurate, it's for file naming
+SERVER_ID = "your-server-id-here"
+DM_CATEGORIES = "Text and Calls"              # Category with DM-style scenes. Has an adjusted export command due to its size
+CATEGORIES_TO_IGNORE = [                      # Categories in the server that are NOT RP and shouldn't be exported and analyzed
+    "Staff Only",
+    "Writing Resources",
+    "RP discussion"
+]
